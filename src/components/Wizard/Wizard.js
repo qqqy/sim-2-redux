9787c03ee438cb91c3e1f6dc4ec859e15b8332d9
qqqy <frogasaurus@hotmail.com>
2018-12-21 10:29:20 -0700

    basic styling complete

---
+++ src/components/Wizard/Wizard.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class Wizard extends Component{
+
+  render(){
+    return(
+      <div className='main'>
+        <div className='sidebar'></div>
+          <div className='mid'>
+            Dashboard, reporting in.
+          </div>
+        <div className='sidebar'></div>
+      </div>
+    )
+  }
+}
+
+export default Wizard
---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import {Switch , Route } from 'react-router-dom'
+import Dashboard from './components/Dashboard/Dashboard'
+import Wizard from './components/Wizard/Wizard'
+import React from 'react'
+
+export default (
+  <Switch>
+    <Route path='/' component={ Dashboard } exact/>
+    <Route path='/wizard' component={ Wizard }/>
+  </Switch>
+)
